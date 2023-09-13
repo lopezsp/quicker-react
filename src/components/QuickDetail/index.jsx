86533699a68687ef1ee121aaa580f24db2b06e29
coias <sebapl09@gmail.com>
2023-09-12 19:17:33 -0500

    refactoring css aside

--- src/components/QuickDetail/index.jsx
+++ src/components/QuickDetail/index.jsx
@@ -35,7 +35,7 @@ const QuickDetail = () => {
     <aside
       className={`${
         context.isQuickDetailOpen ? "flex" : "hidden"
-      } quick-detail flex flex-col fixed bg-white left-0 border border-black rounded-lg p-6`}
+      } quick-detail flex flex-col absolute bg-white left-0 border border-black rounded-lg p-6`}
     >
       <div className="flex justify-between items-center">
         <h2 className="font-medium text-xl">Quick {context.quickInfo.quick_id}</h2>
--- src/components/UserDetail/index.jsx
+++ src/components/UserDetail/index.jsx
@@ -60,7 +60,7 @@ const UserDetail = () => {
     <aside
       className={`${
         context.isUserOpen ? "flex" : "hidden"
-      } user-detail flex flex-col fixed bg-white left-0 border border-black rounded-lg p-6`}
+      } user-detail flex flex-col absolute bg-white left-0 border border-black rounded-lg p-6`}
     >
       <div className="flex justify-between items-center">
         <h2 className="font-medium text-xl">{context.userToShow.nick_name}</h2>
